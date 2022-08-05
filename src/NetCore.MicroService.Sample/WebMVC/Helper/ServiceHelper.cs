6fca6bb3641a6a6a43cc8de1f603d88c63272417
JeremyWu917 <1280442096@qq.com>
2022-08-05 22:18:05 +0800

    complete multi microservice sample

--- src/NetCore.MicroService.Sample/WebMVC/Helper/ServiceHelper.cs
+++ src/NetCore.MicroService.Sample/WebMVC/Helper/ServiceHelper.cs
@@ -7,9 +7,16 @@ namespace WebMVC.Helper
         public async Task<string> GetOrder()
         {
             //订单服务的地址，可以放在配置文件或者数据库等等...
-            string serviceUrl = "http://172.29.12.155:5001";
+            //string serviceUrl = "http://172.29.12.155:5001";
 
-            var Client = new RestClient(serviceUrl);
+            //var Client = new RestClient(serviceUrl);
+
+
+            //服务集群
+            string[] serviceUrls = { "http://172.29.12.155:5101", "http://172.29.12.155:5102", "http://172.29.12.155:5103" };
+            //每次随机访问一个服务实例
+            var Client = new RestClient(serviceUrls[new Random().Next(0, 3)]);
+
             var request = new RestRequest("/orders", Method.Get);
 
             var response = await Client.ExecuteAsync(request);
@@ -19,9 +26,15 @@ namespace WebMVC.Helper
         public async Task<string> GetProduct()
         {
             //产品服务的地址，可以放在配置文件或者数据库等等...
-            string serviceUrl = "http://172.29.12.155:5000";
+            //string serviceUrl = "http://172.29.12.155:5000";
 
-            var Client = new RestClient(serviceUrl);
+            //var Client = new RestClient(serviceUrl);
+
+            //服务集群
+            string[] serviceUrls = { "http://172.29.12.155:5001", "http://172.29.12.155:5002", "http://172.29.12.155:5003" };
+            //每次随机访问一个服务实例
+            var Client = new RestClient(serviceUrls[new Random().Next(0, 3)]);
+
             var request = new RestRequest("/products", Method.Get);
 
             var response = await Client.ExecuteAsync(request);
