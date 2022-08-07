50815cafc8ed631ea389829a9b5768a6423552e3
JeremyWu917 <1280442096@qq.com>
2022-08-07 13:28:50 +0800

    complete consul service automatic find

--- src/NetCore.MicroService.Sample/WebMVC/Helper/ServiceHelper.cs
+++ src/NetCore.MicroService.Sample/WebMVC/Helper/ServiceHelper.cs
@@ -1,9 +1,17 @@
-﻿using RestSharp;
+﻿using Consul;
+using RestSharp;
 
 namespace WebMVC.Helper
 {
     public class ServiceHelper : IServiceHelper
     {
+        private readonly IConfiguration _configuration;
+
+        public ServiceHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async Task<string> GetOrder()
         {
             //订单服务的地址，可以放在配置文件或者数据库等等...
@@ -13,13 +21,33 @@ namespace WebMVC.Helper
 
 
             //服务集群
-            string[] serviceUrls = { "http://localhost:5001", "http://localhost:5002", "http://localhost:5003", "http://localhost:5004", "http://localhost:5005" };
+            //string[] serviceUrls = { "http://localhost:5001", "http://localhost:5002", "http://localhost:5003", "http://localhost:5004", "http://localhost:5005" };
             //每次随机访问一个服务实例
-            var Client = new RestClient(serviceUrls[new Random().Next(0, 3)]);
+            //var Client = new RestClient(serviceUrls[new Random().Next(0, 3)]);
 
+            //var request = new RestRequest("/orders", Method.Get);
+
+            var consulClient = new ConsulClient(c =>
+            {
+                //consul地址
+                c.Address = new Uri(_configuration["ConsulSetting:ConsulAddress"]);
+            });
+            //健康的服务
+            var services = consulClient.Health.Service("OrderService", null, true, null).Result.Response;
+            //订单服务地址列表
+            string[] serviceUrls = services.Select(p => $"http://{p.Service.Address + ":" + p.Service.Port}").ToArray();
+
+            if (!serviceUrls.Any())
+            {
+                return await Task.FromResult("【订单服务】服务列表为空");
+            }
+
+            //每次随机访问一个服务实例
+            var Client = new RestClient(serviceUrls[new Random().Next(0, serviceUrls.Length)]);
             var request = new RestRequest("/orders", Method.Get);
 
             var response = await Client.ExecuteAsync(request);
+
             return response.Content;
         }
 
@@ -31,9 +59,29 @@ namespace WebMVC.Helper
             //var Client = new RestClient(serviceUrl);
 
             //服务集群
-            string[] serviceUrls = { "http://localhost:5006", "http://localhost:5007", "http://localhost:5008", "http://localhost:5009", "http://localhost:5010" };
+            //string[] serviceUrls = { "http://localhost:5006", "http://localhost:5007", "http://localhost:5008", "http://localhost:5009", "http://localhost:5010" };
             //每次随机访问一个服务实例
-            var Client = new RestClient(serviceUrls[new Random().Next(0, 3)]);
+            //var Client = new RestClient(serviceUrls[new Random().Next(0, 3)]);
+
+
+
+            var consulClient = new ConsulClient(c =>
+            {
+                //consul地址
+                c.Address = new Uri(_configuration["ConsulSetting:ConsulAddress"]);
+            });
+            //健康的服务
+            var services = consulClient.Health.Service("ProductService", null, true, null).Result.Response;
+            //产品服务地址列表
+            string[] serviceUrls = services.Select(p => $"http://{p.Service.Address + ":" + p.Service.Port}").ToArray();
+
+            if (!serviceUrls.Any())
+            {
+                return await Task.FromResult("【产品服务】服务列表为空");
+            }
+
+            //每次随机访问一个服务实例
+            var Client = new RestClient(serviceUrls[new Random().Next(0, serviceUrls.Length)]);
 
             var request = new RestRequest("/products", Method.Get);
 
