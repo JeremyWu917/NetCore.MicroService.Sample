6ab2d0643f01805c94f3ca8e69c7a91a3ca1c4f6
JeremyWu917 <1280442096@qq.com>
2022-08-08 22:07:24 +0800

    add ocelot api getway sample

---
+++ src/NetCore.MicroService.Sample/OcelotApiGetway/Program.cs
@@ -0,0 +1,22 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 添加 ocelot 服务
+builder.Services.AddOcelot();
+
+// 加载 json 文件
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile("ocelot.json");
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+// 设置 ocelot 中间件
+app.UseOcelot().Wait();
+
+app.Run();
