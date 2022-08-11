6e8cc0d095a359d40d14558075b3ff29784cbf98
JeremyWu917 <1280442096@qq.com>
2022-08-11 23:04:01 +0800

    add order CAP support

--- src/NetCore.MicroService.Sample/OrderApi/Controllers/OrdersController.cs
+++ src/NetCore.MicroService.Sample/OrderApi/Controllers/OrdersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+using OrderApi.MessageDto;
+using OrderApi.Models;
 
 namespace OrderApi.Controllers
 {
@@ -8,11 +11,21 @@ namespace OrderApi.Controllers
     {
         private readonly ILogger<OrdersController> _logger;
         private readonly IConfiguration _configuration;
+        private readonly ICapPublisher _capBus;
+        private readonly OrderContext _context;
 
-        public OrdersController(ILogger<OrdersController> logger, IConfiguration configuration)
+        //public OrdersController(ILogger<OrdersController> logger, IConfiguration configuration)
+        //{
+        //    _logger = logger;
+        //    _configuration = configuration;
+        //}
+
+        public OrdersController(ILogger<OrdersController> logger, IConfiguration configuration, ICapPublisher capPublisher, OrderContext context)
         {
             _logger = logger;
             _configuration = configuration;
+            _capBus = capPublisher;
+            _context = context;
         }
 
         [HttpGet]
@@ -22,6 +35,32 @@ namespace OrderApi.Controllers
                 $"{Request.HttpContext.Connection.LocalIpAddress}:{_configuration["ConsulSetting:ServicePort"]}";
 
             return Ok(result);
+        }
+
+        /// <summary>
+        /// 下单 发布下单事件
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [Route("Create")]
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order)
+        {
+            using var trans = _context.Database.BeginTransaction(_capBus, autoCommit: true);
+            //业务代码
+            order.CreateTime = DateTime.Now;
+            _context.Orders.Add(order);
+
+            var r = await _context.SaveChangesAsync() > 0;
+
+            if (r)
+            {
+                //发布下单事件
+                await _capBus.PublishAsync("order.services.createorder", new CreateOrderMessageDto() { Count = order.Count, ProductID = order.ProductID });
+                return Ok();
+            }
+            return BadRequest();
+
         }
     }
 }
---
+++ src/NetCore.MicroService.Sample/OrderApi/Models/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderApi.Models
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options)
+           : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+
+}
