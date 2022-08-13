6a890c6abd31e81c64fbf87b26d33ea2bd7cbfe5
JeremyWu917 <1280442096@qq.com>
2022-08-14 00:18:18 +0800

    add identityServer4 sample

---
+++ src/NetCore.MicroService.Sample/Api1Resource/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// 添加授权相关
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            //IdentityServer地址
+            options.Authority = "http://localhost:5001";
+            //对应Idp中ApiResource的Name
+            options.Audience = "api1";
+            //不使用https
+            options.RequireHttpsMetadata = false;
+        });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//身份验证
+app.UseAuthentication();
+
+//授权
+app.UseAuthorization();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/doc/Day6 - IdentityServer4.md
@@ -0,0 +1,7 @@
+# IdentityServer4
+
+> `IdentityServer4` 是为 `ASP.NET Core` 系列量身打造的一款基于 `OpenID Connect` 和 `OAuth 2.0` 认证的框架
+> 
+> `IdentityServer4` 官方文档：[https://identityserver4.readthedocs.io/](https://identityserver4.readthedocs.io/)
+
+
