2f67b9d5cb53ac09fd51b44901299e24fa21e234
JeremyWu917 <1280442096@qq.com>
2022-08-13 11:43:56 +0800

    fix issues

--- src/NetCore.MicroService.Sample/OrderApi/Models/Order.cs
+++ src/NetCore.MicroService.Sample/OrderApi/Models/Order.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OrderApi.Models
 {
+    [Table("Order")]
     public class Order
     {
         [Key]
--- src/NetCore.MicroService.Sample/ProductApi/Models/Product.cs
+++ src/NetCore.MicroService.Sample/ProductApi/Models/Product.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductApi.Models
 {
+    [Table("Product")]
     public class Product
     {
         [Key]
